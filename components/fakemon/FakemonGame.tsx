7e941d9f91db23eb741081c6b27aee076746e36d
badano-nicolas <badano.nicolas@gmail.com>
2022-05-18 21:11:29 -0300

    Added keypress functionality

--- components/fakemon/FakemonGame.tsx
+++ components/fakemon/FakemonGame.tsx
@@ -1,10 +1,14 @@
 import React, { useEffect } from "react";
 import mapImage from "../../public/img/fakemon/towns/town-0.png";
+import player from "../../public/img/fakemon/chars/main/down.png";
 
 const FakemonGame = () => {
   useEffect(() => {
     const canvas = document.querySelector("canvas");
     const canvasContext = canvas?.getContext("2d");
+
+    // Add event listener and detect what key it was
+    document.addEventListener("keydown", detectKeyDown, true);
 
     if (canvas) {
       canvas.width = 1024;
@@ -12,17 +16,52 @@ const FakemonGame = () => {
       canvasContext?.fillRect(0, 0, canvas?.width, canvas?.height);
 
       const image = new Image();
-
       image.src = mapImage.src;
+
+      const playerImage = new Image();
+      playerImage.src = player.src;
+
       image.onload = () => {
-        canvasContext?.drawImage(image, 0, 0);
+        canvasContext?.drawImage(image, -570, -160);
+        canvasContext?.drawImage(
+          playerImage,
+          0,
+          0,
+          playerImage.width / 4,
+          playerImage.height,
+          canvas.width / 2 - playerImage.width / 4,
+          canvas.height / 2 - playerImage.height / 2,
+          playerImage.width / 4,
+          playerImage.height
+        );
       };
     }
   }, []);
+
+  const detectKeyDown = (event: any) => {
+    const key = event.key;
+    switch (key) {
+      case "w":
+        console.log("pressed w");
+        break;
+      case "s":
+        console.log("pressed s");
+        break;
+      case "d":
+        console.log("pressed d");
+        break;
+      case "a":
+        console.log("pressed a");
+        break;
+      default:
+        break;
+    }
+  };
+
   
 
   return (
-    <div>
+    <div className="pl-16">
       <canvas></canvas>
     </div>
   );
